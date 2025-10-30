6b0f3cbef4ce8dd7341561ad608c55c7e4b654ee
itc-s24013 <s24013@std.it-college.ac.jp>
2025-10-30 15:04:47 +0900

    使ってないコンポーネントを削除

--- app/_components/Header/index.tsx
+++
@@ -1,8 +0,0 @@
-import styles from "./index.module.css";
-export default function Header() {
-    return (
-        <header className={styles.header}>
-            <h1 className={styles.title}>ガイカレ祭スタンプラリー</h1>
-        </header>
-    );
-}
